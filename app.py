0bc9dd4d8291a314f00f61b239c4a9f4350348ec
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-02-26 07:51:59 +0530

    small api added

--- app.py
+++ app.py
@@ -1,47 +1,35 @@
-from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, render_template, request
+from crud import getJsonofState
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///dt.db"
-app.config['SQLALCHEMY_DATABASE_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-class paraMeter(db.Model):
-
-    sno = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(30),nullable=False)
-
-    def __repr__(self) -> str:
-        return f"{self.sno} - {self.name}"
+list_of_available_states = ["India", "Bihar", "Chandigarh", "Delhi"]
 
 
-@app.route("/",methods=['GET','POST'])
-
+@app.route("/", methods=["GET", "POST"])
 def home():
-    if request.method=='POST':
-        fname = request.form['fname']
-        lname = request.form['lname']
-        mail = request.form['mail']
-        print(fname,lname,mail)
-    
-    # user = paraMeter(name="Tushar")
-    # db.session.add(user)
-    # db.session.commit()
-    allParams= paraMeter.query.all()
+    if request.method == "POST":
+        a = request.form["state"]
+        return str(a)
+    else:
+        return render_template("index.html", states=list_of_available_states)
 
-    return render_template('index.html',allParams=allParams)
+
+@app.route("/api/<string:state>")
+def appi(state):
+    ret = getJsonofState(state)
+    return jsonify(ret)
+
 
 @app.route("/about")
-
 def about():
     return "About Us"
 
-@app.route("/api-json")
 
+@app.route("/api-json")
 def api():
     return app.send_static_file("dt.json")
 
-if(__name__=="__main__"):
+
+if __name__ == "__main__":
     app.run(debug=True, port=8000)
--- crud.py
+++ crud.py
@@ -1,41 +1,4 @@
 import pandas as pd
-# Andra_Pradesh=pd.read_csv()
-# Arunachal_Pradesh=pd.read_csv()
-# Assam=pd.read_csv()
-# Bihar=pd.read_csv()
-# Chhattisgarh=pd.read_csv()
-# Goa=pd.read_csv()
-# Gujrat=pd.read_csv()
-# Haryana=pd.read_csv()
-# Himachal_Pradesh=pd.read_csv()
-# Jharkhand=pd.read_csv()
-# Karnataka=pd.read_csv()
-# Kerala=pd.read_csv()
-# Madhya_Pradesh=pd.read_csv()
-# Maharashtra=pd.read_csv()
-# Manipur=pd.read_csv()
-# Meghalya=pd.read_csv()
-# Mizoram=pd.read_csv()
-# Nagaland=pd.read_csv()
-# Odisha=pd.read_csv()
-# Punjab=pd.read_csv()
-# Rajasthan=pd.read_csv()
-# Sikkim=pd.read_csv()
-# Tamil_Nadu=pd.read_csv()
-# Telangana=pd.read_csv()
-# Tripura=pd.read_csv()
-# Uttar_Pradesh=pd.read_csv()
-# Uttarakhand=pd.read_csv()
-# West_Bengal=pd.read_csv()
-# Andaman_and_Nicobar=pd.read_csv()
-# Chandigarh=pd.read_csv()
-# Delhi=pd.read_csv()
-# Jammu_and_Kashmir=pd.read_csv()
-# Ladakh=pd.read_csv()
-# Lakshdweep=pd.read_csv()
-# Puducherry=pd.read_csv()
-# Daman_and_Diu=pd.read_csv()
-
 import os
 import json
 
@@ -45,14 +8,14 @@ def getJsonofState(state):
 
     parentPath = os.path.abspath(os.getcwd())+'\\static\\data\\'
     listOfFiles = [x for x in os.listdir(parentPath) if (x.endswith('.csv'))]
-
+    returnJson='Not Found'
     for idx,i in enumerate(listOfFiles):
         if i == state+'.csv':
             df = pd.read_csv('static\data\\'+i)
             df = df.T
-            jsonFile = os.path.abspath(os.getcwd())+'\\static\\dt.json'
-            with open(jsonFile,'a') as outfile:
-                outfile.write(df.to_json())
+            returnJson = df.to_json()
+            return returnJson
+    return returnJson
 
 def getJsonofAllState():
 
