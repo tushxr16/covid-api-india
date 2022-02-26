e34754c42aaecf16021f6339d6356148ac9af090
Tushar Sharma <2020meb1325@iitrpr.ac.in>
2022-02-26 08:20:28 +0530

    now

--- crud.py
+++ crud.py
@@ -5,13 +5,13 @@ import json
 state='Bihar'
 
 def getJsonofState(state):
-    state = str(state)
-    parentPath = os.path.abspath(os.getcwd())+'\\static\\data\\'
+    
+    parentPath = os.path.abspath(os.getcwd())+'/static/data/'
     listOfFiles = [x for x in os.listdir(parentPath) if (x.endswith('.csv'))]
     returnJson='Not Found'
     for idx,i in enumerate(listOfFiles):
         if i == state+'.csv':
-            df = pd.read_csv('static\data\\'+i)
+            df = pd.read_csv('static/data/'+i)
             df = df.T
             returnJson = df.to_json()
             return returnJson
